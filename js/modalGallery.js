6462ca53eea752d9aac27601fc9bbe1213fe2219
hkonsg <hkonsg@gmail.com>
2016-11-13 23:32:50 +0100

    comments

--- js/modalGallery.js
+++ js/modalGallery.js
@@ -1,4 +1,5 @@
-function onClick(element) {
+function onClick(element) { /*Sets the caption in the modal and sets the modal 
+	image to the source and displays as block*/
 	document.getElementById("caption").innerHTML = element.alt;
 	document.getElementById("img01").src = element.src;
 	document.getElementById("pop").style.display = "block";
